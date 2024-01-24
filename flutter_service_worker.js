dd1fd1dd78527bbe273bc1fcd992dd1a28e05c49
Dassine <bl.dassine@gmail.com>
2024-01-24 22:01:19 +0100

    Render multi-urls in paragraph

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "5d0313a8773235ff08f1ba63d2b51581",
-"index.html": "ca8b333f9970169718fca71029a177e1",
-"/": "ca8b333f9970169718fca71029a177e1",
-"main.dart.js": "7949575c02d95d3df887731e00ce0340",
+"index.html": "31fce24ca7e580e189322d94ca5a66cc",
+"/": "31fce24ca7e580e189322d94ca5a66cc",
+"main.dart.js": "5e678324494ead77fd2c37ef86182985",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "fd2b22fbb01720f762673f9c57b1ae52",
 "icons/Icon-192.png": "8eb03f33a8e0db6c324326d6afcac788",
