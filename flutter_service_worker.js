1706e55f06342624d8c35cf37da5e45b10973a9e
Dassine <bl.dassine@gmail.com>
2024-02-08 00:46:30 +0100

    Fix SEO HTML headings issue

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "5d0313a8773235ff08f1ba63d2b51581",
-"index.html": "88c6b43c3e26cf7601f45f4af70f5444",
-"/": "88c6b43c3e26cf7601f45f4af70f5444",
-"main.dart.js": "c4d8e453aac117adc8301e2c3fdaaff7",
+"index.html": "e0c7dadd14049465adf105753960888e",
+"/": "e0c7dadd14049465adf105753960888e",
+"main.dart.js": "661e5555fdd44f02e2d9f1fe2db712bb",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "fd2b22fbb01720f762673f9c57b1ae52",
 "icons/Icon-192.png": "8eb03f33a8e0db6c324326d6afcac788",
