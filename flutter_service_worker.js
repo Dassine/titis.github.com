8f361dc387d90e42243679328481e57f40822a7f
Dassine <bl.dassine@gmail.com>
2024-02-08 22:27:38 +0100

    Restore SEO lang

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "5d0313a8773235ff08f1ba63d2b51581",
-"index.html": "cd7e05fb2917c9a35a4048a3f5dcf2eb",
-"/": "cd7e05fb2917c9a35a4048a3f5dcf2eb",
-"main.dart.js": "64317adaf045ac3c41d5224e91f14f53",
+"index.html": "d4a6629f3f8807a6935a5f213f6f9a73",
+"/": "d4a6629f3f8807a6935a5f213f6f9a73",
+"main.dart.js": "2cc914b37866d2013cf3579c5157623e",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "fd2b22fbb01720f762673f9c57b1ae52",
 "icons/Icon-192.png": "8eb03f33a8e0db6c324326d6afcac788",
